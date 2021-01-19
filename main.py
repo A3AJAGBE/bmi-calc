0aa1bedef6f4de2c4cf22b37f4b0bb92c3e3f9b1
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-19 04:35:58 +0000

    Add bmi calc

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 An application that calculates a user Body Mass Index (BMI) using their weight in kg and height in metres
 """
 print("Weight input should be in 'kg' while Height should be in 'm'")
+
+weight = float(input('Weight: '))
+height = float(input('Height: '))
+
+bmi = round(weight / height ** 2)
+
+print(f'BMI Result: {bmi}')
