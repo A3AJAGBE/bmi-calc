e0ba6cb8055a618b6eb4a3cbf44ccc75e7690971
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-19 04:34:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+"""
+An application that calculates a user Body Mass Index (BMI) using their weight in kg and height in metres
+"""
+print("Weight input should be in 'kg' while Height should be in 'm'")
